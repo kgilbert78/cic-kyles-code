37bd217ae520cb6ce54a8a9b7e4344dc9e25cb02
kgilbert78 <80863489+kgilbert78@users.noreply.github.com>
2021-04-20 10:30:16 -0400

    adjustments to Gus's code from 4-19 class

---
+++ Module-04/Week-01-Day-01/assignment19SmallFair.js
@@ -0,0 +1,22 @@
+const childPrice = 1.5;
+const adultPrice = 4;
+
+const totalEarnings = 5765;
+
+let adults = 2100;
+let children = 0;
+
+let problemIsSolved = false;
+
+while (!problemIsSolved) {
+    const hypotheticalEarnings = (children * childPrice) + (adults * adultPrice);
+    if (hypotheticalEarnings === totalEarnings) {
+        problemIsSolved = true;
+    } else {
+        adults = adults - 1;
+        children = children + 1;
+    }
+}
+
+console.log('Adults: ', adults);
+console.log('Children: ', children);
