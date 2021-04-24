337f7b7cad6ae05ccbc38f34c414cdbf5afc0ee7
kgilbert78 <80863489+kgilbert78@users.noreply.github.com>
2021-04-23 20:10:02 -0400

    fixed extra space after getting prompt working

--- promptTest.js
+++ promptTest.js
@@ -3,7 +3,7 @@ let prompt = require('prompt-sync')();
 // get input from the user.
 //
 let n = prompt('Enter a value: ');
-console.log('You entered ', n);
+console.log('You entered', n);
 
 // To install type: npm i prompt-sync
 //
