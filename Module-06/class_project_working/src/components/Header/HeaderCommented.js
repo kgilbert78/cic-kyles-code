125968bd4c0e56964078dc429509ff459641ec39
kgilbert78 <80863489+kgilbert78@users.noreply.github.com>
2021-06-09 17:01:16 -0400

    added comments to Wk2-Day2 code

--- Module-06/class_project_working/src/components/Header/HeaderCommented.js
+++ Module-06/class_project_working/src/components/Header/HeaderCommented.js
@@ -1,13 +1,70 @@
-// import Link for use in place of <a href=""> which causes whole page to reload, this does not. Only section that is changing.
-import { Link } from 'react-router-dom';
+// import Link for use in place of <a href=""> which causes whole page to reload, this does not. Only section that is changing.import { Link } from "react-router-dom"
+import { Link } from "react-router-dom"
+
+// import scss file to link it instead of <link rel="stylesheet" href="" />
+import "./Header.scss";
+
+/* npm install fontawesome first according to their documentation & what you want to use. Here: npm install --save @fontawesome-svg-core @fontawesome/free-solid-svg-icons  Code below imports fontawesome so it can be referenced as Icon in code for simplicity, then imports the specific icons we're going to use. */
+import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
+import { faSearch, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 // Order important! Header is a child of Router, and since Link is a child of Header this will work.
 export const Header = (props) => {
-    return (
-        <div className="Header">
+  return (
+    <div className="Header">
+        {/* Nav bar adapted from Bootstrap example code https://getbootstrap.com/docs/5.0/examples/navbar-static/ (view page source & copy from <nav> to </nav>, Remove from <form> to </form>, change instances of <a> to <Link>, change instances of class to className, changed classes from -dark to -light) */}
+        <nav className="navbar navbar-expand-md navbar-light bg-light mb-4">
             {/* Link takes prop "to" the way <a> takes href */}
-            <Link to="/">Home</Link>
-            <Link to="/search">Search</Link>
-        </div>
-    )
+            <Link className="navbar-brand" to="/">
+                RandR
+            </Link>         
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarCollapse"
+                aria-controls="navbarCollapse"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+                <ul className="navbar-nav mr-auto ml-auto">
+                    {/* boostrap classes for margin on ul & li to spread nav links */}
+                <li className="nav-item mr-md-5">
+                    <Link className="nav-link" aria-current="page" to="/">
+                    Home
+                    </Link>
+                </li>
+                <li className="nav-item ml-md-5 mr-md-5">
+                    <Link className="nav-link" to="/search">
+                    Shop
+                    </Link>
+                </li>
+                <li className="nav-item ml-md-5">
+                    <Link className="nav-link" to="/wiki">
+                    Wiki
+                    </Link>
+                </li>
+                </ul>
+            </div>
+                {/* Bootstrap classes for don't display search & cart on small screen, display block on medium screen */}
+                <div className="d-none d-md-block">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/search">
+                                <Icon icon={faSearch}/>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/cart">
+                                <Icon icon={faShoppingCart}/>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+        </nav>
+    </div>
+  )
 }
--- Module-06/class_project_working/src/components/Layout/LayoutCommented.js
+++ Module-06/class_project_working/src/components/Layout/LayoutCommented.js
@@ -2,8 +2,10 @@ import { Header } from '../Header/Header';
 import { HomePage } from '../HomePage/HomePage';
 import { SearchPage } from '../SearchPage/SearchPage';
 import { Footer } from '../Footer/Footer';
+import { Wiki } from '../../wiki/Wiki';
+import { CartPage } from '../CartPage/CartPage';
 // BrowserRouter in place of useState for switching what to display because useState gets very complicated when displaying more than 2 things. Refer to BrowserRounter as Router in the code. Easier to type, also useful if you have 2 routers with the same official names.
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
 export const Layout = (props) => {
     return (
@@ -11,12 +13,21 @@ export const Layout = (props) => {
             {/* ^Router must surround everything it interacts with because it's scope is limited to children */}
             <div className="Layout">
                 <Header />
-                {/* Switch accepts children: the different routes you can take with the app */}
+                 {/* Switch accepts children: the different routes you can take with the app */}
                 <Switch>
                     {/* The first route that matches what's in the URL is the one that's going to be shown. Before including path it will look at the URL, which is at the root (without /) and will look for a /. Without seeing /, it will render the first thing it sees that doesn't cause an error, which is SearchPage here. Routes can be self closing also. */}
                     <Route path="/search">
                         <SearchPage />
                     </Route>
+
+                    <Route path="/cart">
+                        <CartPage />
+                    </Route>
+                        
+                    <Route path="/wiki">
+                        <Wiki />
+                    </Route>
+                    
                     {/* without "exact" it will take the first slash it finds after the root URL and stop looking at the rest of the address, so it will display search page */}
                     <Route exact path="/" >
                         <HomePage />
