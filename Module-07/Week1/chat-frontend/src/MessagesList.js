6d6ba3e39a946954089edaf7170b5b65f4041882
kgilbert78 <80863489+kgilbert78@users.noreply.github.com>
2021-06-27 17:04:11 -0400

    changed to async await, attempted timestamp format

--- Module-07/Week1/chat-frontend/src/MessagesList.js
+++ Module-07/Week1/chat-frontend/src/MessagesList.js
@@ -1,3 +1,5 @@
+//import { FormatTimestamp } from "./FormatTimestamp";
+
 export const MessagesList = ({messages, setMessages}) => {
 	return (
 		<div id="messages">
@@ -9,6 +11,7 @@ export const MessagesList = ({messages, setMessages}) => {
 							text={message.text} 
 							timestamp={message.timestamp}
 						/>);
+						// changing line 12 to timestamp={formatDate(message.timestamp)} says RangeError: Invalid time value. date & time seem to be in a different format than in our first chat app. There when I take out the time formatter it displays as Sun Jun 27 2021 11:41:54 GMT-0400 (Eastern Daylight Time) and on the React version it displays as 2021-06-27T15:19:35.565Z - I don't know why, we used new Date() to create them both times. Even so, the website we got the code from https://devhints.io/wip/intl-datetime shows that second format as an ISO string and it looks like it supports it.
 				} else {
 					return (
 						<SentMessage 
@@ -26,38 +29,35 @@ export const MessagesList = ({messages, setMessages}) => {
 
 const SentMessage = (props) => {
 
-	const editClicked = () => {
-		console.log(props.message.text);
+	const editClicked = async () => {
+		//console.log(props.message.text);
 		const editedText = window.prompt(`Enter the new text for your message "${props.message.text}":`);
 		if (editedText) {
-			fetch(`http://localhost:3001/messages/${props.index}`, {
+			const res = await fetch(`http://localhost:3001/messages/${props.index}`, {
 				method: "PUT",
 				headers: {
 					"Content-Type": "application/json"
 				},
 				body: JSON.stringify({text: editedText})
-			}).then((res) => {
-				return res.json();
-			}).then((data) => {
-				props.setMessages(data.messages);
 			});
+			const data = await res.json();
+			props.setMessages(data.messages);
 		};
 	};
 
-	const deleteClicked = () => {
+	const deleteClicked = async () => {
 		window.confirm(`Are you sure you want to delete your message "${props.message.text}"?`)
-		fetch(`http://localhost:3001/messages/${props.index}`, {
+		const res = await fetch(`http://localhost:3001/messages/${props.index}`, {
 			method: "DELETE",
 			headers: {
 				"Content-Type": "application/json"
 			}
-		}).then((res) => {
-			return res.json();
-		}).then((data) => {
-			props.setMessages(data.messages);
 		})
+		const data = await res.json();
+		props.setMessages(data.messages);
 	};
 
+	// return SentMessage
 	return (
 	<div className="row message">
 			<div className="col-2"></div>
@@ -66,7 +66,9 @@ const SentMessage = (props) => {
 			<span onClick={editClicked}>edit</span> | <span onClick={deleteClicked}>delete</span>
 			</div>
 				<span className="messageText">{props.message.text}</span>
+				{/* <FormatTimestamp timestamp={props. message.timestamp} />  */}
 				<div className="timestamp">{props.message.timestamp.toString()}</div>
+				{/* changing line 70 to {formatDate(props.timestamp).toString()} says RangeError: Invalid time value. date & time seem to be in a different format than in our first chat app. There when I take out the time formatter it displays as Sun Jun 27 2021 11:41:54 GMT-0400 (Eastern Daylight Time) and on the React version it displays as 2021-06-27T15:19:35.565Z - I don't know why, we used new Date() to create them both times. Even so, the website we got the code from https://devhints.io/wip/intl-datetime shows that second format as an ISO string and it looks like it supports it. */}
 			</div>
 		</div>
 	);
@@ -83,14 +85,12 @@ const ReceivedMessage = (props) => {
 	);
 };
 
-// // Destructured props:
-// const ReceivedMessage = ({ text, timestamp }) => {
-// 	return (
-// 		<div className="row message">
-// 			<div className="col-10">
-// 				<span className="userMessageText messageText">{text}</span>
-// 				<div className="timestamp">{timestamp.toString()}</div>
-// 			</div>
-// 		</div>
-// 	);
+// function formatDate(timestamp) {
+// 	return new Intl.DateTimeFormat("default", 
+//     {
+// 		hour: "numeric",
+// 		minute: "numeric",
+// 		second: "numeric",
+// 	}
+//     ).format(timestamp);
 // };
