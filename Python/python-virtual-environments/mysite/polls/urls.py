31dea905c25d34b8c8e9f0f46c58d2a17db2bdd3
kgilbert78 <80863489+kgilbert78@users.noreply.github.com>
2021-11-19 23:45:07 -0500

    convert to namespace format in link & add app_name to urls.py

--- Python/python-virtual-environments/mysite/polls/urls.py
+++ Python/python-virtual-environments/mysite/polls/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 
-app_name = 'polls'
+app_name = 'polls' # added this when modifying link in index.html
 urlpatterns = [
     path('', views.index, name='index'), # /polls/ endpoint
     path('<int:question_id>/', views.detail, name='detail'), # for example /polls/5/ supplies 5 to the detail function in views.py using <int:question_id> matched to route parameter. int is converter.
