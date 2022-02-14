b8136d38e6e65d35d3c49a8678801b9ba56e7b3b
kgilbert78 <80863489+kgilbert78@users.noreply.github.com>
2022-02-14 12:32:41 -0500

    added simulated login & theme from last class

--- Module-06/class_project_multiling/src/components/Header/Header.js
+++ Module-06/class_project_multiling/src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import "./Header.scss";
 
@@ -5,12 +6,51 @@ import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
 import { faSearch, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 export const Header = (props) => {
+
+    const [username, setUsername] = useState("Kyle");
+    const [theme, setTheme] = useState("light");
+
+
+    const handleClickLogin = () => {
+        if (username !== "LOGGED OUT") {
+            setUsername("LOGGED OUT")
+        } else {
+            let login = window.prompt("Enter your username:");
+            setUsername(login);
+        }
+    }
+
+    const handleClickTheme = () => {
+        if (theme === "light") {
+            setTheme("dark")
+        } else {
+            setTheme("light")
+        }
+    }
+
     return (
         <div className="Header">
-            <nav className="navbar navbar-expand-md navbar-light bg-light mb-4">
+            <nav
+                className={`navbar navbar-expand-md navbar-${theme} bg-${theme} mb-4`}
+            >
                 <Link className="navbar-brand" to="/">
-                RandR for Username
-                </Link>         
+                    {username !== "LOGGED OUT" ? `R & R for ${username}` : username}
+                </Link>
+
+                <button
+                    className="btn btn-primary btn-sm m-1"
+                    onClick={handleClickLogin}
+                >
+                    {username === "LOGGED OUT" ? "Login" : "Logout"}
+                </button>
+
+                <button
+                    className="btn btn-secondary btn-sm m-1"
+                    onClick={handleClickTheme}
+                >
+                    {theme === "light" ? "GO DARK" : "GO LIGHT"}
+                </button>
+
                 <button
                     className="navbar-toggler"
                     type="button"
@@ -20,44 +60,44 @@ export const Header = (props) => {
                     aria-expanded="false"
                     aria-label="Toggle navigation"
                 >
-                <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="hamburger">
                     <ul className="navbar-nav mr-auto ml-auto">
-                    <li className="nav-item mr-md-5">
-                        <Link className="nav-link" aria-current="page" to="/">
-                        Home
-                        </Link>
-                    </li>
-                    <li className="nav-item ml-md-5 mr-md-5">
-                        <Link className="nav-link" to="/search">
-                        Shop
-                        </Link>
-                    </li>
-                    <li className="nav-item ml-md-5">
-                        <Link className="nav-link" to="/wiki">
-                        Wiki
-                        </Link>
-                    </li>
+                        <li className="nav-item mr-md-5">
+                            <Link className="nav-link" aria-current="page" to="/">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item ml-md-5 mr-md-5">
+                            <Link className="nav-link" to="/search">
+                                Shop
+                            </Link>
+                        </li>
+                        <li className="nav-item ml-md-5">
+                            <Link className="nav-link" to="/wiki">
+                                Wiki
+                            </Link>
+                        </li>
                     </ul>
                 </div>
 
-                    <div className="d-none d-md-block">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/search">
-                                    <Icon icon={faSearch}/>
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/cart">
-                                    <Icon icon={faShoppingCart}/>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
+                <div className="d-none d-md-block">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/search">
+                                <Icon icon={faSearch} />
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/cart">
+                                <Icon icon={faShoppingCart} />
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </nav>
-        </div>
+        </div >
     )
-}
+} 
